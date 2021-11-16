06ff311696d40ad1e49d3125d4595fc39a31943e
KIN Joaquin <kin_08@hotmail.es>
2021-11-16 18:14:00 -0300

    Prefab all

---
+++ Assets/Scripts/EnemyAI/Perceptions/EnvironmentPerception.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentPerception : MonoBehaviour
+{
+    [SerializeField] LayerMask EnvironmentLayer;
+}
---
+++ Assets/Scripts/EnemyAI/States/EnemyStateBase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyStateBase : MonoBehaviour
+{
+    protected EnemyAI Owner;
+
+    public virtual void Init(EnemyAI owner)
+    {
+        this.Owner = owner;
+    }
+
+    public abstract void OnEnter();
+
+    public abstract void OnExit();
+
+    public abstract void Update(float deltaTime);
+
+}
